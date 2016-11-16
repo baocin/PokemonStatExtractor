532280aafc2805466ef06eb0a73d429d6a11d909
Michael Pedersen <steele.pedersen@gmail.com>
2016-11-16 00:39:50 -0500

    Can now extract hp and dust text regions reliably

--- pokemon_stats.m
+++ pokemon_stats.m
@@ -24,12 +24,13 @@ stardust = 600;
 level = [327,165];
 cir_center = [355,457];
 
-%Cache the size since it'll be accessed a lot
-inImageSize = size(img)
+%Resize the input image in order to avoid scale variance
+standardSize = [ 1280 720 ];
+sizeRatio = [ size(img,1)/standardSize(1) size(img,2)/standardSize(2) ];
+img = imresize(img, standardSize);
 
-%The cir_center will always be centered 
-%Column(x position) will always be half of image width
-cir_center(1) = round(inImageSize(2)/2);
+%Cache the size since it'll be accessed a lot
+inImageSize = size(img);
 
 %Mask over the entire card
 pokeMask = im2bw(imread('mask.bmp'));
@@ -37,6 +38,10 @@ pokeMask = im2bw(imread('mask.bmp'));
 dustTemplate= rgb2gray(imread('dust.bmp'));
 %Template of the edit pencil
 editTemplate = rgb2gray(imread('edit.bmp'));
+%Template of the / in the HP
+slashTemplate = imread('slash.bmp');
+%Template of the text "HP"
+hpTemplate = imread('HP.bmp');
 
 %Resize the mask to fit the current image
 pokeMask = imresize(pokeMask, [ inImageSize(1) inImageSize(2) ]);
@@ -44,24 +49,71 @@ maskedRGBImage = bsxfun(@times, img, cast(pokeMask, 'like', img));
 maskedGrayImage = rgb2gray(maskedRGBImage);
 
 %Cut the masked Image into sections where specific templates will be found
+oneFourthCol = round(inImageSize(2)/4);
+oneTenthCol = round(inImageSize(2)/10);
+one100thRow = round(inImageSize(1)/100);
 oneThirdRow = round(inImageSize(1)/3);
-middleThird = maskedGrayImage(oneThirdRow:(oneThirdRow*2), :);
-bottomThird = maskedGrayImage((oneThirdRow*2):end, :);
+oneHalfRow = round(inImageSize(1)/2);
+oneSixthRow = round(inImageSize(1)/6);
 
-% Find Dust 
-dustLocation = template_match(dustTemplate, bottomThird);
-dustLocation(2) = dustLocation(2) + oneThirdRow * 2
-% pause(5);
-    
+cornerDetectionImage = maskedGrayImage(oneThirdRow:end-oneHalfRow, end-oneTenthCol:end);
+textDetectionImage = maskedGrayImage(:,oneFourthCol:(inImageSize(2)-oneFourthCol));
+
+%--------------------------  Detect CIR_Center ---------------------------
+%Detect Corners (For center of the arc's X value)
+corners = detectHarrisFeatures(cornerDetectionImage);
+cornerPoint = corners.selectStrongest(1).Location;
+cir_center(1) = round(inImageSize(2)/2) * sizeRatio(1);
+cir_center(2) = round(cornerPoint(1) + oneThirdRow - (one100thRow*3)) * sizeRatio(2);
+
+%--------------------------  Detect Dust ---------------------------
+% dustLocation = template_match(dustTemplate, bottomThird);
+% %Found the dust icon, now get the text region next to it
+% dustStartRow = round(dustLocation(1));
+% dustEndRow = round(dustLocation(1) + size(dustTemplate,1));
+% dustStartCol = round(dustLocation(2) + size(dustTemplate, 2) * 1.2);
+% dustEndCol = round(dustLocation(2) + size(dustTemplate,2) + (inImageSize(2) * 0.12));
+% bottomThird = textDetectionImage((oneThirdRow*2):end, :);
+% dustTextRegion = bottomThird(dustStartRow:dustEndRow, dustStartCol:dustEndCol);
+
+%--------------------------  Detect HP ---------------------------
+startRow = (cornerPoint(1) + oneThirdRow + (one100thRow * 10));
+endRow = (inImageSize(1)-(oneSixthRow*2) - (one100thRow * 10))
+middleThird = textDetectionImage(startRow:endRow, :);
+hpLocation = template_match(slashTemplate, middleThird);
+%Extract the text region
+hpStartRow = round(hpLocation(1));
+hpEndRow = round(hpLocation(1) + size(slashTemplate,1) * 1.4);
+hpTextRegion = middleThird(hpStartRow:hpEndRow, :);
+
 % %Find Edit
 % editLocation = template_match(editTemplate, middleThird);
-% editLocation(2) = editLocation(2) + oneThirdRow
-% % pause(5);
+% editLocation(2) = editLocation(2) + middleRow
+
+% detect_text(middleThird)
+% pause(5);
+
+% edgeImage = edge(middleThird, 'sobel', 0.05);
+
+% slashLocation = template_match(slashTemplate, edgeImage);
+% slashLocation(2) = slashLocation(2) + middleRow
+
+
 
 %Find Strongest horizontal line in the middle of the image
 %(splits the 
-    
+
+%--------------------------  Testing ---------------------------
+  hFig = figure;
+  hAx  = axes;
+  imshow(hpTextRegion);
+%   hold on;
+%   plot(cir_center(1),cir_center(2),'b*');
+%   hold off;
+  pause(3);
+
 imwrite(maskedGrayImage, sprintf('mask/gray/maskedImage%d.png', i));
+imwrite(middleThird, sprintf('mask/edge/edge%d.png', i));
 imwrite(maskedRGBImage, sprintf('mask/RGB/maskedImage%d.png', i));
 
 end
@@ -74,10 +126,10 @@ function [ position ] = template_match(template, background)
     yoffSet = ypeak-size(template,1);
     xoffSet = xpeak-size(template,2);
     
-    hFig = figure;
-    hAx  = axes;
-    imshow(background,'Parent', hAx);
-    imrect(hAx, [xoffSet+1, yoffSet+1, size(template,2), size(template,1)]);
-    
-    position = [ xpeak(1) ypeak(1) ];
+      hFig = figure;
+      hAx  = axes;
+      imshow(background,'Parent', hAx);
+       imrect(hAx, [xoffSet+1, yoffSet+1, size(template,2), size(template,1)]);
+%     
+    position = [ yoffSet xoffSet ];
 end
